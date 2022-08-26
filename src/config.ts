a74fa238cc64131af3e9f9ca71c04d07f2d3e210
Victor Kane <victorkane@gmail.com>
2022-08-26 15:07:09 -0300

    feat(best Astro practices): Pillage official Astro Blog starter. Homepage begins to consume components to build its layout

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_COMPANY = "AWebFactory"
+export const SITE_NAME = "Project Flow and Tracker"
+export const SITE_SLOGAN1 = "web application factory pattern"
+export const SITE_SLOGAN2 = "workspaces for web application process"
+export const SITE_DESCRIPTION =
+  "A #BuildInPublic #LearnInPublic project-based experience where each purposeful step reports value and is re-usable where needed"
