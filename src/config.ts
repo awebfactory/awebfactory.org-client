765b0bdae4dc5340b9fc9045adbb0fce39d8b6b5
Victor Kane <victorkane@gmail.com>
2022-08-29 05:45:12 -0300

    build(CMS): config file single source of content truth until we hook up CMS

--- src/config.ts
+++ src/config.ts
@@ -8,3 +8,7 @@ export const SITE_SLOGAN2 = "workspaces for web application process"
 export const SITE_DESCRIPTION =
   "A #BuildInPublic #LearnInPublic project-based experience where each purposeful step reports value and is re-usable where needed"
 export const COMPANY_WORKER_OWNER = "Victor Kane"
+export const AWF_INFO1 =
+  "No topic to be isolated, everything must offer value, be done for a purpose, project-based, inserted in its own history, ready to be re-used or pivoted."
+export const AWF_INFO2 =
+  "Victor Kane. Worker/Owner@AWebFactory. Work as praxis in word + code + mentor + poet for real needs. A process engineer and system architect focusing on a lean/UX and agile approach to web application development based on best of class frameworks, communities and tools."
